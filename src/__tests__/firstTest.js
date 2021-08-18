274a002afc3b4b0591df4d0b88ffe912991066f3
Perry Olsson <olsson.perry@gmail.com>
2021-08-18 13:48:12 -0700

    typescript

---
+++ src/__tests__/firstTest.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Index from "../pages/index";
+
+describe("App", () => {
+  it("renders a heading", () => {});
+});
